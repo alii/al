e85cce3e74aedcd0e994c1dce7ff7bac84a398e8
Alistair Smith <hi@alistair.sh>
2025-12-20 18:05:39 +0100

    implement a typed ast

---
+++ CLAUDE.md
@@ -0,0 +1,5 @@
+When building, always use `v .` (make sure you are in the project root before running)
+
+You can do a production build with `v -prod .`
+
+Be sparse when adding comments in the code. Do not add unnecessary comments. Do add comments when explaining larger, more complicated code paths. Especially in things like the parser and compiler or vm.
---
+++ src/compiler/typed_ast/typed_ast.v
@@ -0,0 +1,283 @@
+module typed_ast
+
+import compiler.token
+import compiler.type_def { Type }
+
+pub struct Span {
+pub:
+	line   int
+	column int
+}
+
+pub struct StringLiteral {
+pub:
+	value string
+	span  Span
+}
+
+pub struct InterpolatedString {
+pub:
+	parts []Expression
+}
+
+pub struct NumberLiteral {
+pub:
+	value string
+	span  Span
+}
+
+pub struct BooleanLiteral {
+pub:
+	value bool
+	span  Span
+}
+
+pub struct NoneExpression {}
+
+pub struct ErrorNode {
+pub:
+	message string
+}
+
+pub struct Identifier {
+pub:
+	name string
+	span Span
+}
+
+pub struct TypeIdentifier {
+pub:
+	is_array    bool
+	is_option   bool
+	is_function bool
+	identifier  Identifier
+	param_types []TypeIdentifier
+	return_type ?&TypeIdentifier
+	error_type  ?&TypeIdentifier
+}
+
+pub struct Operator {
+pub:
+	kind token.Kind
+}
+
+pub struct VariableBinding {
+pub:
+	identifier Identifier
+	typ        ?TypeIdentifier
+	init       Expression
+	span       Span
+}
+
+pub struct ConstBinding {
+pub:
+	identifier Identifier
+	typ        ?TypeIdentifier
+	init       Expression
+	span       Span
+}
+
+pub struct FunctionParameter {
+pub:
+	identifier Identifier
+	typ        ?TypeIdentifier
+}
+
+pub struct FunctionExpression {
+pub:
+	identifier  ?Identifier
+	return_type ?TypeIdentifier
+	error_type  ?TypeIdentifier
+	params      []FunctionParameter
+	body        Expression
+}
+
+pub struct IfExpression {
+pub:
+	condition Expression
+	body      Expression
+	span      Span
+	else_body ?Expression
+}
+
+pub struct MatchArm {
+pub:
+	pattern Expression
+	body    Expression
+}
+
+pub struct WildcardPattern {}
+
+pub struct MatchExpression {
+pub:
+	subject Expression
+	arms    []MatchArm
+}
+
+pub struct OrExpression {
+pub:
+	expression    Expression
+	receiver      ?Identifier
+	body          Expression
+	resolved_type Type
+}
+
+pub struct ErrorExpression {
+pub:
+	expression Expression
+}
+
+pub struct PropagateExpression {
+pub:
+	expression    Expression
+	resolved_type Type
+}
+
+pub struct BinaryExpression {
+pub:
+	left  Expression
+	right Expression
+	op    Operator
+	span  Span
+}
+
+pub struct UnaryExpression {
+pub:
+	expression Expression
+	op         Operator
+}
+
+pub struct PostfixExpression {
+pub:
+	expression Expression
+	op         Operator
+}
+
+pub struct ArrayExpression {
+pub:
+	elements []Expression
+	span     Span
+}
+
+pub struct ArrayIndexExpression {
+pub:
+	expression Expression
+	index      Expression
+	span       Span
+}
+
+pub struct RangeExpression {
+pub:
+	start Expression
+	end   Expression
+}
+
+pub struct StructField {
+pub:
+	identifier Identifier
+	typ        TypeIdentifier
+	init       ?Expression
+}
+
+pub struct StructExpression {
+pub:
+	identifier Identifier
+	fields     []StructField
+}
+
+pub struct EnumVariant {
+pub:
+	identifier Identifier
+	payload    ?TypeIdentifier
+}
+
+pub struct EnumExpression {
+pub:
+	identifier Identifier
+	variants   []EnumVariant
+}
+
+pub struct StructInitField {
+pub:
+	identifier Identifier
+	init       Expression
+}
+
+pub struct StructInitExpression {
+pub:
+	identifier Identifier
+	fields     []StructInitField
+}
+
+pub struct PropertyAccessExpression {
+pub:
+	left  Expression
+	right Expression
+}
+
+pub struct FunctionCallExpression {
+pub:
+	identifier Identifier
+	arguments  []Expression
+	span       Span
+}
+
+pub struct BlockExpression {
+pub:
+	body []Expression
+}
+
+pub struct AssertExpression {
+pub:
+	expression Expression
+	message    Expression
+}
+
+pub struct ImportSpecifier {
+pub:
+	identifier Identifier
+}
+
+pub struct ImportDeclaration {
+pub:
+	path       string
+	specifiers []ImportSpecifier
+}
+
+pub struct ExportExpression {
+pub:
+	expression Expression
+}
+
+pub type Expression = ArrayExpression
+	| ArrayIndexExpression
+	| AssertExpression
+	| BinaryExpression
+	| BlockExpression
+	| BooleanLiteral
+	| ConstBinding
+	| EnumExpression
+	| ErrorExpression
+	| ErrorNode
+	| ExportExpression
+	| FunctionCallExpression
+	| FunctionExpression
+	| Identifier
+	| IfExpression
+	| ImportDeclaration
+	| InterpolatedString
+	| MatchExpression
+	| NoneExpression
+	| NumberLiteral
+	| OrExpression
+	| PostfixExpression
+	| PropertyAccessExpression
+	| PropagateExpression
+	| RangeExpression
+	| StringLiteral
+	| StructExpression
+	| StructInitExpression
+	| TypeIdentifier
+	| UnaryExpression
+	| VariableBinding
+	| WildcardPattern
